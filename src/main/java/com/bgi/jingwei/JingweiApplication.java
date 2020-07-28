40a2f8ccf18c209fdc50517494816932ff25f49c
叶远春(Yuanchun Ye) <yeyuanchun@genomics.cn>
2020-07-28 15:42:58 +0800

    'jingwei'

---
+++ src/main/java/com/bgi/jingwei/JingweiApplication.java
@@ -0,0 +1,13 @@
+package com.bgi.jingwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingweiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingweiApplication.class, args);
+	}
+
+}
